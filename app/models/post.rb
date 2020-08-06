67b58bd6604272813ac92d0c70108f6a522e7b57
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-06 03:16:00 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,9 +8,7 @@ class Post < ActiveRecord::Base
     def not_clickbaity_title
       clickbaity_words = ["Won't Believe", "Secret", "Top [number]", "Guess"]
 
-      if !clickbaity_words.include?(self.title)
-
-      else
+      if clickbaity_words.include?(self.title)
         errors.add(:clickbaity_title, "can't be a clickbaity title")
       end
 
