7fad4b5fc55ffcab7f807cc9e6bcb8f7e1e2089f
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-06 03:03:47 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   validates :title, length: { minimum: 1 }
   validates(:content, { :length => { :is => 250 } })
-  validates :category, inclusion: { in: %w(Fiction Non-Fiction), :message "%{value} is not a valid category" }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category" }
   validates :not_clickbaity_title
 
     def not_clickbaity_title
