c14a909bf5c3687b623ccb28315d7f5417381af5
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-06 02:55:05 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,5 @@ class Post < ActiveRecord::Base
   validates(:content, { :length => { :is => 250 } })
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
     message: "%{value} is not a valid category" }
-  validates :not_clickbaity_title
 
-  def not_clickbaity_title
-    if !clickbaity_title.present?
-      valid
-    else
-      errors.add(:clickbaity_title, "can't be a clickbait-y title")
-    end
-  end
 end
